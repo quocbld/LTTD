3407f61c376b92bdeefc3cd84ff12d6f93157bfa
quocbld <quocbldfg@gmail.com>
2022-08-27 21:39:37 +0700

    quoc code

---
+++ app/src/main/java/com/example/lebaquoc_2050531200260/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lebaquoc_2050531200260;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtuser,edtpassword;
+    Button btndangnhap;
+    String ten,mk;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        btndangnhap = (Button) findViewById(R.id.buttondangnhap);
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtuser.getText().length() != 0 && edtpassword.getText().length() !=0){
+                    if (edtuser.getText().toString().equals(ten) && edtpassword.getText().toString().equals(mk)){
+                        Toast.makeText(MainActivity.this,"Bạn đã đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                        startActivity(intent);
+                    }else if (edtuser.getText().toString().equals("quocbld") && edtpassword.getText().toString().equals("12345")){
+                        Toast.makeText(MainActivity.this,"Bạn đã đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(MainActivity.this,"Sai mật khẩu",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"Vui lòng nhập đầy đủ thông tin",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void Anhxa(){
+        edtuser = (EditText) findViewById(R.id.edittextuser);
+        edtpassword = (EditText) findViewById(R.id.edittextpassword);
+        btndangnhap = (Button) findViewById(R.id.buttondangnhap);
+    }
+}
